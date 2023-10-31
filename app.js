4aab9f933491f0d1762b95261ee18f738c5eb739
sadiefiss <sadiefiss@gmail.com>
2023-10-31 16:05:40 -0400

    end of the day updates with plans for tomorrow

--- app.js
+++ app.js
@@ -31,12 +31,32 @@ const emotion = document.getElementById('emotion').value;
 //playerInputs.adjective2Array.push("some adjective2");
 
 
-const poem = `I felt ${playerInputs.emotionArray[0]} as I watched myself ${playerInputs.verbArray[1]} 
-down the ${playerInputs.adjective1Array[2]} path. A ${playerInputs.nounArray[3]} saw me on my way. It is the time of the season I can let 
-myself be ${playerInputs.adjective2Array[4]}.`;
+const poem = `I felt ${playerInputs.emotionArray[0]} as I watched myself ${playerInputs.verbArray[0]} 
+down the ${playerInputs.adjective1Array[0]} path. A ${playerInputs.nounArray[0]} saw me on my way. It is the time of the season I can let 
+myself be ${playerInputs.adjective2Array[0]}.`;
 
 const poemResult = document.getElementById('poem-result');
 poemResult.textContent = poem;
+
+//reset input
+document.getElementById('emotion').value
+document.getElementById('verb').value 
+document.getElementById('adjective1').value
+document.getElementById('noun').value 
+document.getElementById('adjective2').value
+
+
+playerInputs.emotionArray = [];
+    playerInputs.verbArray = [];
+    playerInputs.adjective1Array = [];
+    playerInputs.nounArray = [];
+    playerInputs.adjective2Array =[];
+//i want the input boxes to reset after the code comes in 
+//and i want the poem to not show untill the user clicks to enter it and
+// i would like the button to change color when its hovered over
+//will possibly create more word templets to randomize within my code 
+//then i will fix up my css with pink cloud bac
+
 
 }
 
