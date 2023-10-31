937d10d1512624eabc05e41c883492e76fc1cfe1
sadiefiss <sadiefiss@gmail.com>
2023-10-30 22:27:52 -0400

    mondayevening update

---
+++ app.js
@@ -0,0 +1,41 @@
+console.log("js loaded")
+
+
+
+//constants-
+
+const firstPoem  = {  line1: "I felt",
+                      emotion:"",
+                      line2: "as I watched myself",
+                      verbIng:"",
+                      line3: "down the ",
+                      adjective:"",
+                      line3:"Path. A",
+                      noun:"",
+                      line4: "saw me on my way. It is time of the season I can let my self be",
+                      adjective2:"",
+
+
+
+
+}
+
+
+//startervariables -states the data that will change while the game is running
+
+
+
+
+
+//cache elements
+
+
+
+//event listeners 
+document.getElementById("buttonGen").addEventListener("click", generatePoem);
+
+//functions
+function generatePoem(){ 
+
+
+}
